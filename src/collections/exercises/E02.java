43b7dfc2e101ba87a822c01b1c51637e09d9229a
CabosManuel <cabos.apolaya@gmail.com>
2022-11-15 12:32:13 -0500

    Solution exercise 02

---
+++ src/collections/exercises/E02.java
@@ -0,0 +1,14 @@
+package collections.exercises;
+
+import java.util.ArrayList;
+
+// 2. Write a Java program to iterate through all elements in a array list.
+public class E02 {
+  
+  public static void solution(ArrayList<String> array) {
+    System.out.println("\nExercise 02:");
+    for (String object : array) {
+      System.out.println(object);
+    }
+  }
+}
