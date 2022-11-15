d8e9f8dc60ad0b6d1be420ff23574d873b444333
CabosManuel <cabos.apolaya@gmail.com>
2022-11-15 15:00:55 -0500

    Main class

--- src/collections/App.java
+++ src/collections/App.java
@@ -1,7 +1,21 @@
 package collections;
 
+import collections.arraylist.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+	public static void main(String[] args) throws Exception {
+		
+		E01.solution();
+		E02.solution(E01.getColors());
+		E03.solution(E01.getColors(), "white");
+		E04.solution(E01.getColors(), 1);
+		E05.solution(E01.getColors(), 0, "black");
+		E06.solution(E01.getColors());
+		E07.solution(E01.getColors(), "blue");
+		E08.solution(E01.getColors());
+		E09.solution(E01.getColors());
+		E10.solution(E09.getCopy());
+		E11.solution(E09.getCopy());
+		E12.solution(E09.getCopy());
+	}
 }
